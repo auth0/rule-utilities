93d1a4a96a8cd945e42219d6345fdbebb75e44e9
Josh Cunningham <josh@joshcanhelp.com>
2020-08-14 09:22:47 -0700

    Clear master

--- lint-staged.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  "*.{js,jsx,ts,tsx,md,json}": [
-    "prettier --write"
-  ]
-};
--- prettier.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  "arrowParens": "always",
-  "proseWrap": "always",
-  "requirePragma": false
-};
