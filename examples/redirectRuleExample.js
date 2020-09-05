8a12e03b5fde15f83794d6b5583a64ef8d07d8b4
Josh Cunningham <josh@joshcanhelp.com>
2020-09-05 10:18:49 -0700

    Update example

--- examples/redirectRuleExample.js
+++ examples/redirectRuleExample.js
@@ -13,10 +13,21 @@
 function redirectRuleExample(user, context, callback) {
   const { Auth0RedirectRuleUtilities } = require("@auth0/rule-utilities@0.1.0");
 
+  /*
+  Override or set defaults for configuration values
+  const customConfiguration = {
+    ...configuration,
+    ...{
+      SESSION_TOKEN_SECRET: "custom token secret",
+      SESSION_TOKEN_EXPIRES_IN: "in seconds or a string describing a time span",
+    }
+  }
+  */
+
   const ruleUtils = new Auth0RedirectRuleUtilities(
     user,
     context,
-    configuration
+    configuration // or customConfiguration
   );
 
   if (ruleUtils.isRedirectCallback && ruleUtils.queryParams.session_token) {
@@ -43,6 +54,7 @@ function redirectRuleExample(user, context, callback) {
     try {
       // This method automatically creates a session token.
       // To add data to this token, use ruleUtils.createSessionToken and pass as second param below.
+      // To omit the session token, pass false as second param below.
       ruleUtils.doRedirect(configuration.ID_VERIFICATION_URL);
       callback(null, user, context);
     } catch (error) {
